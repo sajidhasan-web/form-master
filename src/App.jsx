b8030985041366ba4faecaee4f925d91970df0f8
sajidhasan-web <sajidhasan.web@gmail.com>
2024-03-21 21:51:42 +0600

    initail file create and create Simple form component

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     
+      <h1>Vite + React</h1>
+     
+       <form>
+        <input type="text" name="" id="" />
+        <br /><br />
+        <button>Submit</button>
+       </form>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/SimpleForm/SimpleForm.jsx
@@ -0,0 +1,16 @@
+
+
+const SimpleForm = () => {
+    return (
+        <div>
+            <form>
+                <input type="text" />
+                <br /><br />
+                <button>Submit</button>
+            </form>
+            
+        </div>
+    );
+};
+
+export default SimpleForm;
